4b85d45cb2feb420c981cb0f4bb10dea4262e67d
Fadi013 <fadi.sraidi@capgemini.com>
2022-08-31 15:08:58 +0100

    fadi commit 2 Panne

--- src/test/java/Pages/Pannier.java
+++ src/test/java/Pages/Pannier.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Pannier extends Tools {
-    public WebElement driver;
+    public WebDriver driver;
 
     @FindBy(xpath = "//a[contains(@Class,'wpmenucart-contents')]")
     private WebElement panier;
