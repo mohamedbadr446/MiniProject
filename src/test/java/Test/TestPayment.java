baa61dcf7f8c092aff86a339553c1c463431edef
feljorja <farah.el-jorjany@capgemini.com>
2022-09-15 15:52:24 +0100

    Add Payment

--- src/test/java/Test/TestPayment.java
+++ src/test/java/Test/TestPayment.java
@@ -1,7 +1,10 @@
 package Test;
 
 import Connexion.SetUp;
+import Constante.JDD;
+import Pages.LoginPage;
 import Pages.Myaccount;
+import Pages.Pannier;
 import Pages.PaymentPage;
 import com.xpandit.testng.annotations.Xray;
 import org.testng.annotations.Test;
@@ -11,12 +14,25 @@ public class TestPayment extends SetUp {
    @Xray(requirement = "BACBPI-165", test = "BACBPI-346")
     public void run()
     {
+        LoginPage loginPage=new LoginPage(driver);
+        loginPage.clickMyaccount();
+        loginPage.verifierLogo();
+        loginPage.insertLoginEmail(JDD.username);
+        loginPage.insertLoginPwd(JDD.pwd);
+        loginPage.clickButtonLogin();
+        loginPage.verfierPageLogin(JDD.msgPage);
+
+
         PaymentPage payment =new PaymentPage(driver);
-        payment.cliqueShopPay();
-        payment.cliqueUpdateBasket();
+        //Ajouter Produit au pannier
+        Pannier pannier=new Pannier(driver);
+        pannier.clickShop();
+        // pannier.clickProduit();
+        String price= pannier.getPrice();
+        pannier.clickAddToBasket();
+        pannier.clickViewBasket();
+        pannier.verifierPrix(price);
         payment.cliqueCheckOut();
-        payment.cliqueCheckPayment();
-        payment.cliquePaypalExpress();
         payment.cliqueCashOnDelivery();
         payment.cliquePlaceOrder();
         payment.cliqueOrderDetails();
